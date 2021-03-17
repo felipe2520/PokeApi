47181fda9916074ca8ac00c42b9eef2152465791
Felipe Silva <felipesilva32100@gmail.com>
2021-03-17 13:54:03 -0300

    PokeApi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { api } from './services';
 
 function App() {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+
+    api
+    .get("pokemon")
+    .then(res => console.warn(res))
+    .catch((err) => console.warn(err));
+  },[]);
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h3>Teste</h3>
     </div>
   );
 }
